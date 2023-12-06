d8ce7dc6f33dd8dfa306dc278a06a4866d0efd37
hamzamajeed01 <h.hamza9696hb@gmail.com>
2023-12-06 22:40:58 +0500

    model deployed front end and back end added

--- Codes/app.py
+++ Codes/app.py
@@ -1,4 +1,7 @@
 from flask import Flask, request, render_template
+import joblib
+import pandas as pd
+import numpy as np
 
 app = Flask(__name__)
 
@@ -9,16 +12,61 @@ def index():
 @app.route('/predict_price', methods=['POST'])
 def predict():
     if request.method == 'POST':
-        latitude = request.form['latitude']
-        longitude = request.form['longitude']
-        baths = request.form['baths']
-        bedrooms = request.form['bedrooms']
-        total_area = request.form['total_area']
-        property_type = request.form['property_type']
-        predicted_price = latitude+longitude+baths+bedrooms+total_area+property_type  
-        return render_template('index.html', predicted_price=predicted_price)
-    else:
-        return render_template('index.html', predicted_price=None)
+        try:
+            new_data = {
+                'area': float(request.form['area']),
+                'bedrooms': int(request.form['bedrooms']),
+                'bathrooms': int(request.form['bathrooms']),
+                'stories': int(request.form['stories']),
+                'mainroad': int(request.form['mainroad']),
+                'guestroom': int(request.form['guestroom']),
+                'basement': int(request.form['basement']),
+                'hotwaterheating': int(request.form['hotwaterheating']),
+                'airconditioning': int(request.form['airconditioning']),
+                'parking': int(request.form['parking']),
+                'prefarea': int(request.form['prefarea']),
+                'furnishingstatus_furnished': 1 if request.form['furnishingstatus'] == 'furnished' else 0,
+                'furnishingstatus_semi-furnished': 1 if request.form['furnishingstatus'] == 'semi-furnished' else 0,
+                'furnishingstatus_unfurnished': 1 if request.form['furnishingstatus'] == 'unfurnished' else 0
+                
+            }
+            new_input_data = pd.DataFrame([new_data])
+            loaded_scaler = joblib.load('scaler.joblib')
+            lr= joblib.load("LR_model")
+            loaded_pca_components = joblib.load('pca_components.joblib')
+            new_input_data_normalized = loaded_scaler.transform(new_input_data)
+            new_input_data_pca = np.dot(new_input_data_normalized, loaded_pca_components.T)
+            predicted_price = lr.predict(new_input_data_pca)
+            return render_template('index.html', predicted_price=f'Predicted House Price: {predicted_price[0]:.2f}')
+        except Exception as e:
+            return render_template('index.html', predicted_price=f'Error: {e}')
+# new_data = {
+#     'area': 6000,
+#     'bedrooms': 4,
+#     'bathrooms': 1,
+#     'stories': 2,
+#     'mainroad': 1,
+#     'guestroom': 0,
+#     'basement': 1,
+#     'hotwaterheating': 0,
+#     'airconditioning': 0,
+#     'parking': 3,
+#     'prefarea': 0,
+#     'furnishingstatus_furnished': 0,
+#     'furnishingstatus_semi-furnished': 1,
+#     'furnishingstatus_unfurnished': 0
+# }
+
+# new_input_data = pd.DataFrame([new_data])
+# loaded_scaler = joblib.load('scaler.joblib')
+# loaded_pca_components = joblib.load('pca_components.joblib')
+# new_input_data_normalized = loaded_scaler.transform(new_input_data)
+# new_input_data_pca = np.dot(new_input_data_normalized, loaded_pca_components.T)
+# predicted_price = lr.predict(new_input_data_pca)
+# print(f'Predicted House Price: {predicted_price[0]:.2f}')
+
+
+           
 
 if __name__ == '__main__':
     app.run(debug=True)
